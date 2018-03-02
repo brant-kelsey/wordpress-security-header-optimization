c75fb5db2b99a5a3819c08ac07c43d552b10930a
optimalisatie <info@optimalisatie.nl>
2018-03-02 10:53:45 +0100

    core 0.0.12

--- README.txt
+++ README.txt
@@ -5,7 +5,7 @@ Tags: csp, security, http headers, optimization, content security policy, xss, a
 Requires at least: 4.0
 Requires PHP: 5.4
 Tested up to: 4.9.4
-Stable tag: 0.0.5
+Stable tag: 0.0.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -39,7 +39,7 @@ Documentation is available on [Github](https://github.com/o10n-x/wordpress-secur
 
 == Changelog ==
 
-= 0.0.3 =
+= 0.0.6 =
 
 Core update (see changelog.txt)
 
--- security-header-optimization.php
+++ security-header-optimization.php
@@ -12,7 +12,7 @@ namespace O10n;
  * @wordpress-plugin
  * Plugin Name:       Security Header Optimization
  * Description:       Advanced security header optimization toolkit. Content-Security-Policy, Strict Transport Security (HSTS), Public-Key-Pins (HPKP), X-XSS-Protection and CORS.
- * Version:           0.0.5
+ * Version:           0.0.6
  * Author:            Optimization.Team
  * Author URI:        https://optimization.team/
  * Text Domain:       o10n
@@ -29,8 +29,8 @@ if (defined('WP_INSTALLING') && WP_INSTALLING) {
 }
 
 // settings
-$module_version = '0.0.5';
-$minimum_core_version = '0.0.11';
+$module_version = '0.0.6';
+$minimum_core_version = '0.0.12';
 $plugin_path = dirname(__FILE__);
 
 // load the optimization module loader
